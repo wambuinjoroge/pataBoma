3fe4460d57daa8c4d8b38972876e40659c0b8827
wambuinjoroge <christinenjoroge93@gmail.com>
2017-12-01 12:36:48 +0300

    created houseowner table

--- database/migrations/2017_12_01_090646_add_house_number_to_houses_table.php
+++ database/migrations/2017_12_01_090646_add_house_number_to_houses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateHousesTable extends Migration
+class AddHouseNumberToHousesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreateHousesTable extends Migration
      */
     public function up()
     {
-        Schema::create('houses', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
+        Schema::table('houses', function (Blueprint $table) {
+            $table->integer('house_number');
         });
     }
 
@@ -26,6 +25,8 @@ class CreateHousesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('houses');
+        Schema::table('houses', function (Blueprint $table) {
+            $table->dropColumn('house_number');
+        });
     }
 }
--- database/migrations/2017_12_01_093400_create_house_owner_table.php
+++ database/migrations/2017_12_01_093400_create_house_owner_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateHousesTable extends Migration
+class CreateHouseOwnerTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,9 @@ class CreateHousesTable extends Migration
      */
     public function up()
     {
-        Schema::create('houses', function (Blueprint $table) {
+        Schema::create('house_owner', function (Blueprint $table) {
             $table->increments('id');
+            $table->text('name');
             $table->timestamps();
         });
     }
@@ -26,6 +27,6 @@ class CreateHousesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('houses');
+        Schema::dropIfExists('house_owner');
     }
 }
