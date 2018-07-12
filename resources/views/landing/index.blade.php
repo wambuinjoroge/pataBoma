01c9d5175524fa68a1a1d862d6abdd9866f9c8a0
wambuinjoroge <christinenjoroge93@gmail.com>
2018-07-12 10:19:36 +0300

    logout functionality working and landing page too

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -6,13 +6,15 @@
     <link href="{{ asset('css/custom1.css') }}" rel="stylesheet">
 @endpush
 
-<div class="row col-md-12 col-lg-12 col-xs-12 col-sm-12" id="background">
+{{--<div class="row col-md-12 col-lg-12 col-xs-12 col-sm-12" id="background">--}}
 
-    <img src="assets/images/image.jpg" alt="The house to live!" class="image">
-    <h1 id="header">Pata Home</h1>
+    {{--<img src="{{ asset('images/house2.jpeg') }}" alt="The house to live!" class="image">--}}
 
-    <h3 id="subheader">Providing you with a home for life</h3>
 
-</div>
-    
+{{--</div>--}}
+    <div class="image">
+        {{--<h1 id="header">Pata Home</h1>--}}
+
+        {{--<h3 id="subheader">Providing you with a home for life</h3>--}}
+    </div>
 @stop
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,6 +86,6 @@
     </div>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    {{--<script src="{{ asset('js/app.js') }}"></script>--}}
 </body>
 </html>
